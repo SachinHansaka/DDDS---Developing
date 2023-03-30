56eeb980e13e853238db586352e05669f72f805a
seyonluis143 <100549660+seyonluis143@users.noreply.github.com>
2023-03-30 18:31:19 +0530

    Add files via upload

---
+++ Member 4 (Seyon)/main.dart
@@ -0,0 +1,29 @@
+import 'package:drowse_detector/Screens/Ending_Screen.dart';
+import 'package:drowse_detector/Screens/Splash_Screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+late List<CameraDescription> cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  cameras = await availableCameras();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
